9afac99eb894584dbce6b6ac550ba6fdb02921c7
DougBeckford <119445410+DougBeckford@users.noreply.github.com>
2022-12-05 00:09:36 +0200

    cum cum cum

--- helpers/candyMachineInfo.js
+++ helpers/candyMachineInfo.js
@@ -1,4 +1,4 @@
-export const candyMachineAddress = "0xaae2fdba212b79cac0559a232064e22da1340c1435728bbca0d881a4d84d6361";
+export const candyMachineAddress = "0xf6813afef3ced561e4707fd38782facfe597e23a5729a91dfa2eb1e2530337b2";
 
 export const collectionName = "Test101"; // Case sensitive!
 export const MaxMint = 3
@@ -9,7 +9,7 @@ export const COLLECTION_SIZE = 10
 export const collectionCoverUrl = "https://r2aa42v3uaweghlsqfqb4d3vr6vh7egqqhnb3fqxhnigfxkxlcya.arweave.net/joAOarugLEMdcoFgHg91j6p_kNCB2h2WFztQYt1XWLA";
 export const collectionBackgroundUrl = "https://6wnm7eahnmrz3w3a55pu627ibxus563nsfaykske3uuteyj5h77q.arweave.net/9ZrPkAdrI53bYO9fT2voDeku-22RQYVJRN0pMmE9P_8";
 
-export const mode = "dev"; // "dev" or "test" or "mainnet"
+export const mode = "test"; // "dev" or "test" or "mainnet"
 export let NODE_URL;
 export const CONTRACT_ADDRESS = "0x5b71b400de0767bcec88464c33a0c74c839737206883a9379252f4907b8bf30e";
 export const SERVICE_NAME = "ftmpad"
